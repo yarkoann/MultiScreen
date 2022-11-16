fafd8c9c079b815e7d4468b4ac3af8cc6539ca1c
Yarovenko <yarovenko@307-07.local>
2022-11-17 08:55:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    String[] countries = { "Аргентина", "Бразилия", "Чили", "Колумбия", "Уругвай", "Яровенко Анна"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        setListAdapter(adapter);
+
+        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Intent intent1 = new Intent(MainActivity.this, ArgentinaActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case 1:
+                        Intent intent2 = new Intent(MainActivity.this, BrazilActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case 2:
+                        Intent intent3 = new Intent(MainActivity.this, ChilyActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case 3:
+                        Intent intent4 = new Intent(MainActivity.this, ColumbyActivity.class);
+                        startActivity(intent4);
+                        break;
+                    case 4:
+                        Intent intent5 = new Intent(MainActivity.this, UrugbaiActivity.class);
+                        startActivity(intent5);
+                        break;
+                    case 5:
+                        Intent intent6 = new Intent(MainActivity.this, YarovenkoActivity.class);
+                        startActivity(intent6);
+                        break;
+                }
+                Toast.makeText(getApplicationContext(),"Вы выбрали" + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+        getListView().setOnItemClickListener(itemListener);
+    }
+}
